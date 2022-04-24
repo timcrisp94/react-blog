00af68d1a078a09949f297b94f181a2d0f221ee6
Tim Crisp <timothycrisp@gmail.com>
2022-04-24 11:50:09 -0500

    handle error in blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,12 +4,13 @@ import useFetch from './useFetch'
 const BlogDetails = () => {
   const { id } = useParams()
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+  const history = useHistory()
 
   const handleClick = () => {
     fetch('http://localhost:8000/blogs/'+ blog.id, {
       method: 'DELETE'
-    }) .then(() => {
-        history.push('/')
+    }).then(() => {
+      history.push('/')
     })
   }
 
